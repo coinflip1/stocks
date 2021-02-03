d2d43979668168205bc70ef32395233cc693ed16
coinflip1 <73563987+coin@users.noreply.github.com>
2021-02-03 08:21:31 -0500

    axios

--- src/Stats.js
+++ src/Stats.js
@@ -1,6 +1,45 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import './Stats.css';
+import axios from 'axios';
 
 function Stats() {
+
+const BASE_URL='https://finnhub.io/api/v1/quote?symbol="'
+
+const [stockData, setStockData] = useState([])
+
+const getStockData =()=>{
+    return axios
+    .get(`${BASE_URL}${stock}${KEY_URL}`)
+    .catch((error) => {
+      console.error("Error", error.message);
+    });
+    }
+
+useEffect (()=>{
+    const stocksList = ["AAPL", "MSFT", "TSLA", "FB", "BABA", "UBER", "DIS", "SBUX"];
+
+    getMyStocks();
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock)
+        .then((res) => {
+          testData.push({
+            name: stock,
+            ...res.data
+          });
+        })
+      )
+    });
+
+    Promise.all(promises).then(()=>{
+      console.log(testData);
+      setStocksData(testData);
+    })
+
+},[])
+
     return (
         <div className='stats'>
             <div className='stats__container'>
