7808ac1ccfe035b30285570c76c911c09443d2fa
coinflip1 <73563987+coin@users.noreply.github.com>
2021-02-02 23:12:28 -0500

    graph js

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Newsfeed.css';
-
+import LineGraph from './LineGraph';
 function Newsfeed() {
     return (
         <div className='newsfeed'>
@@ -10,7 +10,9 @@ function Newsfeed() {
         <h1>$114,656</h1>
 <p>$44.63 (+0.04%) Today</p>
     </div>
-
+<div className="newsfeed__chart">
+<LineGraph/>
+</div>
 </div>
             </div>
            
--- src/Stats.js
+++ src/Stats.js
@@ -3,7 +3,23 @@ import React from 'react'
 function Stats() {
     return (
         <div className='stats'>
-            
+            <div className='stats__container'>
+                <div className="stats__header">
+                    <p>Stocks</p>
+
+</div>
+<div className='stats__content'>
+<div className='stats__row'>
+
+
+                </div>
+
+</div>
+<div className="stats__header">
+                    <p>List</p>
+
+</div>
+            </div>
         </div>
     )
 }
